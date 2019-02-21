e63fcbf8395bf4c16ee46f8e6c67ebd589f90548
Vadim Storozhuk <vadozy@yahoo.com>
2019-02-21 07:57:21 -0500

    End of lecture 49

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -4,6 +4,11 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
+import { Link, hashHistory } from 'react-router';
+
+import query from '../queries/fetchSongs'
+
+
 class SongCreate extends Component {
 
   constructor(props) {
@@ -17,13 +22,17 @@ class SongCreate extends Component {
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
-    });
+      },
+      refetchQueries: [{ query }]
+    })
+      .then(() => hashHistory.push('/'))
+      .catch(() => {});
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -4,6 +4,10 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
+import { Link } from 'react-router';
+
+import query from '../queries/fetchSongs'
+
 class SongList extends Component {
 
   renderSongs() {
@@ -19,20 +23,19 @@ class SongList extends Component {
     console.log(this.props);
 
     return (
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
+      <div>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+        <Link
+          to="/songs/new"
+          className="btn-floating btn-large red right"
+        >
+        <i className="material-icons">add</i>
+        </Link>
+      </div>
     );
   }
 }
-
-const query = gql(`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`);
 
 export default graphql(query)(SongList);
